3828f5fc9baa282930b0318ab02ae67c37538ece
lenind9 <lenindgc@hotmail.com>
2024-02-21 20:57:35 -0500

    Autorización y autenticación con Spring Boot

---
+++ src/main/java/com/uce/edu/demo/service/UsuarioServiceImpl.java
@@ -0,0 +1,26 @@
+package com.uce.edu.demo.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import com.uce.edu.demo.repository.IUsuarioRepository;
+import com.uce.edu.demo.repository.modelo.Usuario;
+
+import static java.util.Collections.emptyList;
+
+public class UsuarioServiceImpl implements UserDetailsService {
+	
+	@Autowired
+	private IUsuarioRepository usuarioRepository;
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		// TODO Auto-generated method stub
+		Usuario usuario = this.usuarioRepository.consultarPorNombre(username);
+		return new User(usuario.getNombre(), usuario.getPassword(), emptyList());
+	}
+
+}
